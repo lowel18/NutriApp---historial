9a48f57bb23eb513b706a385eb31cbdcf060b82f
Roberto Carlos <a20171016@pucp.edu.pe>
2021-11-05 19:12:34 -0500

    historial funciona del 5/11

--- nutriapp/lib/mainScreen.dart
+++ nutriapp/lib/mainScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:nutriapp/lista_hist.dart';
 
 class pantallaPrincipal extends StatefulWidget {
   pantallaPrincipal({Key? key}) : super(key: key);
@@ -35,6 +36,38 @@ class _pantallaPrincipalState extends State<pantallaPrincipal> {
     );
   }
 
+  Material myItemTap(String ruta){
+    return Material(
+      color: Colors.white38,
+      shadowColor: Color(0x802196F3),
+      borderRadius: BorderRadius.circular(24),
+      child: Card(
+        clipBehavior: Clip.antiAlias,
+        shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(24)
+                      ),
+        elevation: 4.0,
+        child: ClipRRect(
+                        borderRadius: BorderRadius.circular(24),
+                        child: Container(
+                          child: Ink.image( 
+                            image: NetworkImage(ruta),
+                            fit: BoxFit.cover,
+                            child: InkWell(
+                              onTap: (){
+                                      print('funciona el on tap');
+                                      //ListaHistorial2();
+                              },
+                            ),
+                          ),
+                          
+                        ),
+                      ),
+      ),
+      
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final Size tam = MediaQuery.of(context).size;
@@ -50,9 +83,9 @@ class _pantallaPrincipalState extends State<pantallaPrincipal> {
             mainAxisSpacing: alto*0.03,
             padding: EdgeInsets.symmetric(horizontal: 16.0,vertical: 8.0),
             children: [
-              myItem('https://i.ibb.co/QjzwtVJ/bg-1.jpg'),
-              myItem('https://i.ibb.co/LzVd2VD/Rainbow-fruits-berries-background-on-white-Top-view-of-strawberries-blueberries-cherries-mango-apple.jpg'),
-              myItem('https://i.ibb.co/MC8K8c4/bg-fruta-5-2.jpg'),
+              myItemTap('https://i.ibb.co/QjzwtVJ/bg-1.jpg'),
+              myItemTap('https://i.ibb.co/LzVd2VD/Rainbow-fruits-berries-background-on-white-Top-view-of-strawberries-blueberries-cherries-mango-apple.jpg'),
+              myItemTap('https://i.ibb.co/MC8K8c4/bg-fruta-5-2.jpg'),
             ],
             staggeredTiles: [
               StaggeredTile.extent(2, alto*0.35),
